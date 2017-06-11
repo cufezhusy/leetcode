4cfc31af94692e14244aef41b18262bace2f8b7b
Shengyao Zhu <shengyao.zhu@gmail.com>
2017-06-11 10:16:26 +0200

    test case for package six

---
+++ self_study/test_package_six.py
@@ -0,0 +1,20 @@
+# Six provides simple utilities for wrapping over differences between Python 2 and Python 3.
+# It is intended to support codebases that work on both Python 2 and 3 without modification.
+# six consists of only one Python file, so it is painless to copy into a project.
+
+import six
+import unittest
+
+
+# Six contains compatibility shims for unittest assertions that have been renamed. The parameters are the same
+#  as their aliases, but you must pass the test method as the first argument. For example:
+class TestAssertCountEqual(unittest.TestCase):
+    def test(self):
+        six.assertCountEqual(self, (1, 2), [2, 1])
+
+class TestDataType(unittest.TestCase):
+    def test(self):
+        self.assertEqual(isinstance(6, six.integer_types),True)
+        self.assertEqual(isinstance(6.6, six.integer_types), False)
+        self.assertEqual(isinstance("shengyao", six.string_types),True)
+        self.assertEqual(isinstance('shengyao', six.string_types), True)
